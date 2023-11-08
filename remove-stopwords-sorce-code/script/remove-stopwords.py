04ebe6177aefbc65aef598b1387436fa46a5ff80
Dyego Henrique Costa Braga <dyego.hcb@gmail.com>
2023-11-08 19:17:38 -0300

    CORRECAO REMOVE ESPACO BRANCO DE STOPWORDS REMOVIDAS

--- remove-stopwords-sorce-code/script/remove-stopwords.py
+++ remove-stopwords-sorce-code/script/remove-stopwords.py
@@ -4,15 +4,20 @@ from nltk.corpus import stopwords
 nltk.download('stopwords')
 stop_words_pt = set(stopwords.words('portuguese'))
 
+
 def remove_stop_words(text):
     words = text.split()
-    filtered_text = ' '.join(word for word in words if word.lower() not in stop_words_pt)
+    filtered_text = ' '.join(
+        word for word in words if word.lower() not in stop_words_pt)
     return filtered_text
+
 
 def process_file(input_file_path, output_file_path):
     with open(input_file_path, 'r', encoding='utf-8') as f_in, open(output_file_path, 'w', encoding='utf-8') as f_out:
         for line in f_in:
-            f_out.write(remove_stop_words(line) + '\n')
+            if remove_stop_words(line).strip(): 
+                f_out.write(remove_stop_words(line) + '\n')
+
 
 input_file_path = './input/in.txt'
 output_file_path = './output/out.txt.txt'
